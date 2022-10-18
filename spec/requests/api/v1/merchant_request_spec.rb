034f2b334b8ba44377f8c50bb8e9e0a7fbd5f86d
Thomas Hayes <hayes.thomas610@gmail.com>
2022-10-18 07:58:34 -0600

    Add to request spec to test for keys and add model validation

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -4,10 +4,22 @@ describe "Merchants API" do
   context "GET /api/v1/merchants" do
     it "sends a list of merchants" do
       create_list(:merchant, 3)
-      
+
       get '/api/v1/merchants'
-  
+
       expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.count).to eq(3)
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(Integer)
+  
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).to be_a(String)
+      end
     end
   end
 end
