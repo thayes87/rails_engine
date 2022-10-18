d0a056835c4b730eadf1d9f388a6e6c2e95f56d4
Thomas Hayes <hayes.thomas610@gmail.com>
2022-10-18 13:13:54 -0600

    create spec, factory, route for item index; create spec and route for merchant show

---
+++ spec/factories/item.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Coffee.blend_name }
+    description { Faker::Lorem.sentence(word_count: 3) }
+    unit_price { Faker::Number.decimal(l_digits: 2) }
+  end
+end
