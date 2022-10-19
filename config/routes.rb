ecfb73c93e1725e442e8422d0c3befe4af7e7882
Thomas Hayes <hayes.thomas610@gmail.com>
2022-10-19 17:21:15 -0600

    Add item_merchant controller and request spec

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do 
         resources :items, only: [:index], controller:  'merchants_items'
       end
-      resources :items
+      resources :items do
+        resource :merchant, only: [ :show], controller: 'item_merchant'
+      end
     end
   end
 end
